226cb4ef85443176ee68c3a2927950cfa7e1a847
Михеев Максим Павлович <maksim.mikheev@rubius.com>
2026-09-06 19:08:00 +0300

    added UserDao interface and implementation

---
+++ src/main/java/user/service/dao/UserDao.java
@@ -0,0 +1,13 @@
+package user.service.dao;
+
+import user.service.entity.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void save(User user);
+    User findById(Long id);
+    List<User> findAll();
+    void update(User user);
+    void delete(Long id);
+}
